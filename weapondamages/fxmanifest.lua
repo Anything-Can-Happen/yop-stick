f44ffbbdf744317f877e370cdf99a5d35f59f02e
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2022-12-21 14:49:30 -0500

    Update fxmanifest.lua

--- weapondamages/fxmanifest.lua
+++ weapondamages/fxmanifest.lua
@@ -1,9 +1,16 @@
 fx_version 'cerulean'
 
+author 'Best & Other Authors'
+description 'Weapon Damages'
+version '1'
+
 files {
   'weapons.meta'
 }
 
 data_file 'WEAPONINFO_FILE_PATCH' 'weapons.meta'
 
-client_script 'client.lua'
+client_scripts {
+    'config.lua'
+    'client.lua',
+}
